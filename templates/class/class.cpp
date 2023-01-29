78a3b556c7d822ca53666f0a5366f74203be3e99
Theo Crandall <thrandale@gmail.com>
2023-01-28 20:33:58 -0700

    fixed pico config

--- templates/class/class.cpp
+++ templates/class/class.cpp
@@ -4,8 +4,3 @@ NEW_CLASS_NAME::NEW_CLASS_NAME()
 {
     // Constructor
 }
-
-NEW_CLASS_NAME::~NEW_CLASS_NAME()
-{
-    // Destructor
-}
--- templates/class/class.h
+++ templates/class/class.h
@@ -1,11 +1,11 @@
 #ifndef NEW_CLASS_NAME_UPPER_H
 #define NEW_CLASS_NAME_UPPER_H
+#include "pico/stdlib.h"
 
 class NEW_CLASS_NAME
 {
 public:
     NEW_CLASS_NAME();
-    ~NEW_CLASS_NAME();
 
 private:
     // Private members
